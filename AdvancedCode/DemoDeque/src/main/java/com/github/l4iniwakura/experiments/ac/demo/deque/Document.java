43f093b9f881847b01fd19b2f355ba67a7400c68
redeyes <s3rzh@inbox.ru>
2025-11-04 23:06:27 +0700

    chore: -

--- AdvancedCode/DemoDeque/src/main/java/com/github/l4iniwakura/experiments/ac/demo/deque/Document.java
+++
@@ -1,9 +0,0 @@
-package com.github.l4iniwakura.experiments.ac.demo.deque;
-
-import java.util.UUID;
-
-public record Document(
-        UUID id,
-        double score
-) {
-}
--- AdvancedCode/DemoDeque/src/main/java/com/github/l4iniwakura/experiments/ac/demo/deque/User.java
+++
@@ -1,8 +0,0 @@
-package com.github.l4iniwakura.experiments.ac.demo.deque;
-
-import java.util.UUID;
-
-public record User(
-        UUID userId
-) {
-}
